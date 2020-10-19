592b91f7f56184ae049a29df6d2921f3e777d250
markgalante <mark.galante.14@gmail.com>
2020-10-19 20:50:30 +0200

    added mongo db to app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/kinetic", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+mongoose.connection.once("open", () => console.log("connected to database")); 
 
 app.listen(3000, () => console.log("server listening on port 3000"));
