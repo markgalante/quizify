0f4e9192571f14412856dbf207da1608396254df
markgalante <mark.galante.14@gmail.com>
2020-10-22 19:53:08 +0200

    corrected schema declaration

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,34 +1,76 @@
 const { graphqlHTTP } = require("express-graphql");
 const graphql = require("graphql");
 
+//Import of Mongoose Schemas: 
+const Quiz = require("../models/quiz");
+const Question = require("../models/question");
+const Option = require("../models/option");
+
 const {
     GraphQLObjectType,
+    GraphQLList,
+    GraphQLSchema,
     GraphQLID,
     GraphQLString,
     GraphQLBoolean
 } = graphql;
 
-const QuizType = new GraphQLObjectType({
-    name: "Quiz",
+const OptionType = new GraphQLObjectType({
+    name: "Option",
     fields: () => ({
-        id: { GraphQLID },
-        title: { GraphQLString },
+        id: { type: GraphQLID },
+        option: { type: GraphQLString },
+        isCorrect: { type: GraphQLBoolean }
     })
 });
 
 const QuestionType = new GraphQLObjectType({
     name: "Question",
     fields: () => ({
-        id: { GraphQLID },
-        question: { GraphQLString },
+        id: { type: GraphQLID },
+        question: { type: GraphQLString },
+        options: {
+            type: new GraphQLList(OptionType),
+            resolve(parent, args) {
+                return Option.find({ questionId: parent.id });
+            },
+        },
     })
 });
 
-const OptionType = new GraphQLObjectType({
-    name: "Option",
+const QuizType = new GraphQLObjectType({
+    name: "Quiz",
     fields: () => ({
-        id: { GraphQLID },
-        option: { GraphQLString },
-        isCorrect: { GraphQLBoolean }
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        questions: {
+            type: new GraphQLList(QuestionType),
+            resolve(parent, args) {
+                return Question.find({ quizId: parent.id });
+            }
+        }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        quiz: {
+            type: QuizType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Quiz.findById(args.id);
+            },
+        },
+        quizes: {
+            type: QuizType,
+            resolve(parent, args) {
+                return Quiz.find();
+            }
+        },
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
 }); 
