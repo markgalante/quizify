b9b3aefe60d7a5b309a0ae165d5f28cda5497e67
markgalante <mark.galante.14@gmail.com>
2020-10-24 21:19:44 +0200

    Add cors to app.js on client side

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
 
 const app = express();
+app.use(cors()); 
 
 mongoose.connect("mongodb://localhost/quizify", {
     useNewUrlParser: true,
