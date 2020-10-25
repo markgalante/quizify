16024d91fddeabebdc4eb5b64ef4b587a37b774b
markgalante <mark.galante.14@gmail.com>
2020-10-25 12:38:06 +0200

    Add route to get to quiz route

--- client/src/components/QuizList.jsx
+++ client/src/components/QuizList.jsx
@@ -3,25 +3,41 @@ import React from "react";
 import { GET_QUIZZES } from "../graphql/queries";
 import LoadingSpinner from "./LoadingSpinner";
 import Error from "./Error";
+import Quiz from "./Quiz";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 const QuizList = () => {
     const { loading, data, error } = useQuery(GET_QUIZZES);
 
     return (
         <div className="quiz-list">
-            {
-                loading
-                    ? (<LoadingSpinner />)
-                    : error
-                        ? <Error message={error.message}/>
-                        : <ul>
-                            {
-                                data.quizes.map(quiz => (
-                                    <li key={quiz.id}><a href={`/${quiz.id}`}>{quiz.title}</a></li>
-                                ))
-                            }
-                        </ul>
-            }
+            <Router>
+                {
+                    loading
+                        ? (<LoadingSpinner />)
+                        : error
+                            ? <Error message={error.message} />
+                            : <ul>
+                                <Route>
+                                    {
+                                        data.quizes.map(quiz => (
+                                            <li key={quiz.id}><Link to={`${quiz.id}`}>{quiz.title}</Link></li>
+                                        ))
+                                    }
+                                </Route>
+                            </ul>
+                }
+
+                <Switch>
+                    <Route path="/:id"><Quiz /></Route>
+                </Switch>
+            </Router>
+
         </div>
     )
 };
