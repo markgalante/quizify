7a31ad9102f67f00bce420fa45a4e70b6162c0ea
markgalante <mark.galante.14@gmail.com>
2020-10-25 20:28:35 +0200

    Get each quiz on a page

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -3,19 +3,39 @@ import { GET_QUIZ } from "../graphql/queries";
 import { useQuery } from "@apollo/client"
 import { useRouteMatch } from "react-router-dom";
 
+import LoadingSpinner from "./LoadingSpinner";
+import Error from "./Error"; 
+
 const Quiz = () => {
     let match = useRouteMatch();
     const { data, loading, error, variables } = useQuery(GET_QUIZ, {
         variables: {
-            id: match.params.id, 
+            id: match.params.id,
         }
     });
     console.log({ loading, data, variables });
+    if (data) console.log(data.quiz.title);
     if (error) console.error({ error });
+    console.log({ match });
 
-    console.log({ match })
+    const question = (
+            data.quiz.questions.map(question => (
+            <div key={question.id}>{question.question}</div>
+            ))
+        );
+    console.log({question})
     return (
-        <div>Quiz</div>
+        <div>
+            {
+                loading
+                    ? <LoadingSpinner />
+                    : error
+                        ? <Error />
+                        : (
+                            <h1>{data.quiz.title}</h1>
+                        )
+            }
+        </div>
     );
 };
 
