e211fb62ecafbd5ccba33ac65bb5555a0496d27c
markgalante <mark.galante.14@gmail.com>
2020-10-26 20:10:30 +0200

    Route between home and page to add/create quiz

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
 //Imports of components 
-import QuizList from "./components/QuizList"; 
+import QuizList from "./components/QuizList";
+import AddQuiz from "./components/AddQuiz";
 
 function App() {
   return (
     <div className="App">
-      <QuizList /> 
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/create">Create Quiz</Link></li>
+            </ul>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/"><QuizList /></Route>
+          <Route path="/create"><AddQuiz /> </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/AddQuiz.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+const AddQuiz = () => {
+    return(
+        <div>
+            Add Quiz
+        </div>
+    ); 
+};
+
+export default AddQuiz; 
