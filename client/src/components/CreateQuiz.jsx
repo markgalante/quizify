dc93ff888f86ea7d8b742259783517d845f1b320
markgalante <mark.galante.14@gmail.com>
2020-10-26 21:29:09 +0200

    Add title of quiz

--- client/src/components/CreateQuiz.jsx
+++ client/src/components/CreateQuiz.jsx
@@ -1,11 +1,30 @@
-import React from "react"; 
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { CREATE_QUIZ } from "../graphql/mutations";
 
 const CreateQuiz = () => {
-    return(
+    const [title, setTitle] = useState('');
+    const [createQuiz, { data, error }] = useMutation(CREATE_QUIZ);
+    function handleSubmit(e) {
+        e.preventDefault();
+        createQuiz({
+            variables: {
+                title,
+                creatorId: "5f9400bfa74aa73a3cee98ac",
+            }
+        });
+        setTitle('');
+        if (error) console.log(error)
+        console.log({ data, error });
+    }
+    return (
         <div>
-            CreateQuiz
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={e => setTitle(e.target.value)} required/>
+                <button type="submit">Create Quiz</button>
+            </form>
         </div>
-    ); 
+    );
 };
 
 export default CreateQuiz; 
---
+++ client/src/graphql/mutations.js
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_QUIZ = gql`
+    mutation CreateQuiz($title: String!, $creatorId: ID!){
+        createQuiz(title: $title, creatorId: $creatorId){
+            id
+            title
+        }
+    }
+`; 
