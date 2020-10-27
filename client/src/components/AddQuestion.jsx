351f5b6731c7a889125e7fadb5878ddbce75e9c0
markgalante <mark.galante.14@gmail.com>
2020-10-27 20:39:45 +0200

    Logic for add question to Quiz

--- client/src/components/AddQuestion.jsx
+++ client/src/components/AddQuestion.jsx
@@ -1,11 +1,31 @@
-import React from "react"; 
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_QUESTION } from "../graphql/mutations"
 
-const AddQuestion = () => {
-    return(
+const AddQuestion = ({ id }) => {
+    const [question, setQuestion] = useState('');
+    const [addQuestion, { data }] = useMutation(ADD_QUESTION);
+    function handleSubmit(e) {
+        e.preventDefault();
+        addQuestion({
+            variables: {
+                question,
+                quizId: id
+            }
+        });
+        setQuestion(''); 
+    };
+    console.log({ data })
+    console.log({ question, addQuestion })
+    return (
         <div>
-            AddQuestion.jsx
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="question">Add question</label>
+                <input id="question" onChange={e => setQuestion(e.target.value)} />
+                <button type="submit">Add Question</button>
+            </form>
         </div>
     )
-}; 
+};
 
 export default AddQuestion; 
