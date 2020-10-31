8637e064a083f55567050a48d97a5386231f7e1f
markgalante <mark.galante.14@gmail.com>
2020-10-31 21:35:56 +0200

    Use refetchQueries property to update list of quizzes

--- client/src/components/CreateQuiz.jsx
+++ client/src/components/CreateQuiz.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { CREATE_QUIZ } from "../graphql/mutations";
+import { GET_QUIZZES } from "../graphql/queries";
 
 const CreateQuiz = () => {
     const [title, setTitle] = useState('');
     const [createQuiz, { data, error }] = useMutation(CREATE_QUIZ);
+
     function handleSubmit(e) {
         e.preventDefault();
         createQuiz({
             variables: {
                 title,
                 creatorId: "5f9400bfa74aa73a3cee98ac",
-            }
+            },
+            refetchQueries: [{ query: GET_QUIZZES }]
         });
         setTitle('');
         if (error) console.log(error)
@@ -20,7 +23,7 @@ const CreateQuiz = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" onChange={e => setTitle(e.target.value)} required/>
+                <input type="text" onChange={e => setTitle(e.target.value)} value={title} required />
                 <button type="submit">Create Quiz</button>
             </form>
         </div>
