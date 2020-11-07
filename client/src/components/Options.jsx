9ddf0aa0ae69bdc00cb9cd9456c60fb50e9d85ff
markgalante <mark.galante.14@gmail.com>
2020-11-07 10:42:42 +0200

    Fetch options from gql

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddOption from "./AddOption";
+import LoadingSpinner from "./LoadingSpinner";
+import Error from "./Error";
+import { useQuery } from "@apollo/client";
+import { SHOW_OPTIONS } from "../graphql/queries";
 
-const Options = ({ options, questionId }) => {
-    const answerOptions = options.map(option => (
-        <div key={option.id}>
-            <input type="radio" id={option.option} name="answer" value={option.option} /> <label>{option.option}</label>
-        </div>
-    ));
+const Options = ({ questionId }) => {
+    const { data, loading, error } = useQuery(SHOW_OPTIONS, {
+        variables: {
+            questionId
+        }
+    });
+
+
+    const [showOptions, setOptions] = useState(null);
+    const [pageLoading, setLoading] = useState(true);
+    const [fetchError, setError] = useState(null);
+    
+    useEffect(() => {
+        if (!loading) setLoading(false);
+        if (data) setOptions(data);
+        if (error) setError(error.message);
+    }, [loading, data, error]);
 
     return (
         <div>
             <form>
-                {answerOptions}
+                {
+                    pageLoading
+                        ? <LoadingSpinner />
+                        : showOptions
+                            ? showOptions.options.map(option => (
+                                <div key={option.id}>
+                                    <input type="radio" id={option.option} name="answer" value={option.option} /> <label>{option.option}</label>
+                                </div>
+                            ))
+                            : <Error message={fetchError.message} />
+                }
             </form>
-            <AddOption options={options} questionId={questionId} /> 
+            {
+                showOptions 
+                ? (
+                    <AddOption options={showOptions.options} questionId={questionId} />
+                ) 
+                : null
+            }
+            
+
         </div>
     );
 };
