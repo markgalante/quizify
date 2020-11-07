146218e04f9ee80f25b62d1a52ec63e9c94313cf
markgalante <mark.galante.14@gmail.com>
2020-11-07 12:31:14 +0200

    implement update quiz name logic into front-end

--- client/src/components/Quiz.jsx
+++ client/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GET_QUIZ } from "../graphql/queries";
 import { useQuery } from "@apollo/client"
 import { useRouteMatch, Link, Switch, Route, BrowserRouter as Router } from "react-router-dom";
@@ -7,14 +7,20 @@ import LoadingSpinner from "./LoadingSpinner";
 import Error from "./Error";
 import Questions from "./Questions";
 import AddQuestion from "./AddQuestion";
+import UpdateQuizName from "./UpdateQuizName";
 
 const Quiz = () => {
     let match = useRouteMatch();
+    const [edit, showEdit] = useState(false);
     const { data, loading, error } = useQuery(GET_QUIZ, {
         variables: {
             id: match.params.id,
         }
     });
+
+    useEffect(() => {
+        showEdit(false);
+    }, [data])
 
     return (
         <div>
@@ -25,7 +31,16 @@ const Quiz = () => {
                         ? <Error />
                         : (
                             <div>
-                                <h1>{data.quiz.title}</h1>
+                                {
+                                    edit
+                                        ? (<UpdateQuizName title={data.quiz.title} id={match.params.id} edit={edit} />)
+                                        : (
+                                            <div>
+                                                <h1 onDoubleClick={() => showEdit(true)}>{data.quiz.title}</h1>
+                                            </div>
+                                        )
+                                }
+
                                 <Questions questions={data.quiz.questions} />
                                 <Router>
                                     <ul>
