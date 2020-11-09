71239d38ae71ff6d6ff136ac482c541b9c405289
markgalante <mark.galante.14@gmail.com>
2020-11-09 19:34:46 +0200

    Logic added to update question

--- client/src/components/UpdateQuestion.jsx
+++ client/src/components/UpdateQuestion.jsx
@@ -1,12 +1,30 @@
-import React from "react"; 
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { UPDATE_QUESTION } from "../graphql/mutations";
+import { QUESTION_LIST } from "../graphql/queries";
 
-const UpdateQuestion = ({question, id, editQuestion}) => {
-    console.log({question, id, editQuestion}); 
-    return(
-        <form>
-            <input type="test"/>
+const UpdateQuestion = ({ question, id, editQuestion }) => {
+    const [updateQuestion] = useMutation(UPDATE_QUESTION);
+    const [updatedQuestion, setQuestion] = useState(question);
+    function handleSubmit(e) {
+        console.log({question, editQuestion});
+        e.preventDefault();
+        updateQuestion({
+            variables: {
+                id,
+                question: updatedQuestion,
+            },
+            refetchQueries: [{ query: QUESTION_LIST, variables: { id } }]
+        });
+        editQuestion = false;
+        setQuestion("");
+        console.log({question, id, editQuestion}); 
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={updatedQuestion || ""} onChange={e => setQuestion(e.target.value)} />
         </form>
-    ); 
-}; 
+    );
+};
 
 export default UpdateQuestion; 
