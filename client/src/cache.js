b2cbe36c14bda527b844d85365946bf1094007ce
markgalante <mark.galante.14@gmail.com>
2020-11-10 19:43:14 +0200

    Rename constants

--- client/src/cache.js
+++ client/src/cache.js
@@ -1,5 +1,5 @@
 import { makeVar } from "@apollo/client"; 
 
-export const showUpdateQuiz = makeVar(false); 
+export const showQuizEdit = makeVar(false); 
 export const showQuestionEdit = makeVar(false);
-export const showUpdateOptions = makeVar(false);  
+export const showOptionsEdit = makeVar(false);  
