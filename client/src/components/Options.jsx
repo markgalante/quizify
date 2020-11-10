de9f233a6b58ed77790e2aa1f82e1e6701df718a
markgalante <mark.galante.14@gmail.com>
2020-11-10 19:57:05 +0200

    To show if no options have been given.

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -16,7 +16,7 @@ const Options = ({ questionId }) => {
     const [showOptions, setOptions] = useState(null);
     const [pageLoading, setLoading] = useState(true);
     const [fetchError, setError] = useState(null);
-    
+
     useEffect(() => {
         if (!loading) setLoading(false);
         if (data) setOptions(data);
@@ -30,22 +30,24 @@ const Options = ({ questionId }) => {
                     pageLoading
                         ? <LoadingSpinner />
                         : showOptions
-                            ? showOptions.options.map(option => (
-                                <div key={option.id}>
-                                    <input type="radio" id={option.option} name="answer" value={option.option} /> <label>{option.option}</label>
-                                </div>
-                            ))
+                            ? showOptions.options.length
+                                ? showOptions.options.map(option => (
+                                    <div key={option.id}>
+                                        <input type="radio" id={option.option} name="answer" value={option.option} /> <label>{option.option}</label>
+                                    </div>
+                                ))
+                                : <em>No options given</em>
                             : <Error message={fetchError.message} />
                 }
             </form>
             {
-                showOptions 
-                ? (
-                    <AddOption options={showOptions.options} questionId={questionId} />
-                ) 
-                : null
+                showOptions
+                    ? (
+                        <AddOption options={showOptions.options} questionId={questionId} />
+                    )
+                    : null
             }
-            
+
 
         </div>
     );
