962e98e637484521a5ee450fa627fd2c6f0eefc0
markgalante <mark.galante.14@gmail.com>
2020-11-11 21:15:40 +0200

    Add to UpdateOption component

--- client/src/components/UpdateOption.jsx
+++ client/src/components/UpdateOption.jsx
@@ -1,20 +1,33 @@
-import React from "react";
-import { useMutation } from "@apollo/client";
-import { showOptionsEdit } from "../cache";
-import { UPDATE_OPTION } from "../graphql/mutations";
-import { SHOW_OPTIONS } from "../graphql/queries";
+import React, { useState } from "react";
+// import { useMutation } from "@apollo/client";
+// import { showOptionsEdit } from "../cache";
+// import { UPDATE_OPTION } from "../graphql/mutations";
+// import { SHOW_OPTIONS } from "../graphql/queries";
 
 const UpdateOption = ({ options }) => {
+    const [updatedOption, setOption] = useState("");
+    const [updatedIsCorrect, setIsCorrect] = useState(false);
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e);
+    }
+    
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             { options.map(option => (
                 <div key={option.id}>
-                    <input type="radio" id={option.option} name="answer" value={option.option} checked={option.isCorrect} />
-                    <input type="text" value={option.option} /> 
+                    <input
+                        type="radio"
+                        id={option.option}
+                        onChange={e => setIsCorrect(!updatedIsCorrect)}
+                        value={option.option}
+                    />
+                    <input type="text" value={option.option} onChange={e => setOption(e.target.value)} />
                 </div>
-
             ))}
-
+            <button type="submit">Update</button>
         </form>
     );
 };
