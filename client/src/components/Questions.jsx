3d6ef279a72df95ce5880e43b07a8215f16c7aab
markgalante <mark.galante.14@gmail.com>
2020-11-15 19:35:15 +0200

    useMutation used to delete questions.
    Reload "quiz" query, pass params.id as the variable.

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Options from "./Options";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { showQuestionEdit } from "../cache";
-import { useReactiveVar } from "@apollo/client"
+import { useReactiveVar, useMutation } from "@apollo/client";
+import { DELETE_QUESTION } from "../graphql/mutations";
+import { QUESTION_LIST } from "../graphql/queries";
 
 import UpdateQuestion from "./UpdateQuestion";
 
 const Questions = ({ questions }) => {
+    const [deleteQuestion] = useMutation(DELETE_QUESTION);
     const editQuestion = useReactiveVar(showQuestionEdit);
     const location = useLocation();
+    const params = useParams();
+
+    function handleDeleteQuestion(questionId) {
+        deleteQuestion({
+            variables: { questionId },
+            refetchQueries: [{ query: QUESTION_LIST, variables: { id: params.id } }]
+        });
+    }
     return (
         <div>
             {
@@ -18,7 +29,9 @@ const Questions = ({ questions }) => {
                             {
                                 editQuestion
                                     ? <UpdateQuestion question={question.question} questionId={question.id} editQuestion={editQuestion} />
-                                    : <h3 onDoubleClick={() => showQuestionEdit(true)}>{question.question} <span className="delete-button">X</span></h3>
+                                    : <h3 onDoubleClick={() => showQuestionEdit(true)}>
+                                        {question.question} <span className="delete-button" onClick={() => handleDeleteQuestion(question.id)}>X</span>
+                                    </h3>
                             }
                             <div className="options" onClick={() => showQuestionEdit(false)}>
                                 <Options options={question.options} questionId={question.id} path={location.pathname} />
