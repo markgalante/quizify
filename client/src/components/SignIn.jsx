d331bb56253e2a5a2d689a2926eaab582b4ef37b
markgalante <mark.galante.14@gmail.com>
2020-11-22 16:55:39 +0200

    Set up login/signin form

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,11 +1,25 @@
-import React from "react"; 
+import React, { useState } from "react";
 
 const SignIn = () => {
-    return(
-        <div>
-            SignIn
-        </div>
-    ); 
-}; 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    return (
+        <form action="/login" method="POST">
+            <div>
+                <label>Email</label>
+                <input type="text" onChange={e => setEmail(e.target.value)} />
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" onChange={e => setPassword(e.target.value)} />
+            </div>
+
+            <button
+                type="submit"
+                disabled={!email || !password}
+            >Submit</button>
+        </form>
+    );
+};
 
 export default SignIn; 
