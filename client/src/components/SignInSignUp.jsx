0886f720546e32ce98fc659090ae159e6aa4cfb2
markgalante <mark.galante.14@gmail.com>
2020-11-22 12:48:30 +0200

    Create page with SignInSignUp Logic

---
+++ client/src/components/SignInSignUp.jsx
@@ -0,0 +1,15 @@
+import React from "react"; 
+
+import SignIn from "./SignIn"; 
+import SignUp from "./SignUp"; 
+
+const SignInSignUp = () => {
+    return(
+        <div>
+            <SignIn /> 
+            <SignUp /> 
+        </div>
+    )
+}
+
+export default SignInSignUp; 
