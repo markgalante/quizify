1a9123e3eeb1e8bf159ef4a7ca9ac3f64c995843
markgalante <mark.galante.14@gmail.com>
2020-11-22 16:21:41 +0200

    Add link to sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 //Imports of components 
 import QuizList from "./components/QuizList";
 import CreateQuiz from "./components/CreateQuiz";
+import SignInSignUp from "./components/SignInSignUp";
 
 function App() {
   return (
@@ -14,12 +15,14 @@ function App() {
             <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/create">Create Quiz</Link></li>
+              <li><Link to="/signin">Sign In</Link></li>
             </ul>
           </nav>
         </div>
         <Switch>
           <Route exact path="/"><QuizList /></Route>
           <Route path="/create"><CreateQuiz /> </Route>
+          <Route path="/signin"><SignInSignUp /></Route>
         </Switch>
       </Router>
     </div>
