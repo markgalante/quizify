cf615cd2fb10f9aacc997bb04aa98bc0686080c8
markgalante <mark.galante.14@gmail.com>
2020-11-22 16:24:13 +0200

    Remove users from session

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -5,10 +5,6 @@ export const GET_QUIZZES = gql`
         quizes {
             id
             title
-            creator {
-                id
-                name
-            }
         }
     }
 `;
