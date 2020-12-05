282ab00dec7b67425680f3d10c09a426049ef64e
markgalante <mark.galante.14@gmail.com>
2020-12-05 15:24:13 +0200

    Add Lougout logic

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -5,7 +5,6 @@ const passport = require("passport");
 router.post("/login",
     (req, res, next) => {
         passport.authenticate("local", (err, user, info) => {
-            console.log("BEFORE", req.user);
             if (err) {
                 console.log("ERROR RUNNIGN LOGIN", err)
                 return res.status(400).json({ errors: err });
@@ -16,13 +15,17 @@ router.post("/login",
             }
             req.logIn(user, (err) => {
                 if (err) return res.status(400).json({ errors: err });
-                // console.log("Logged on as", req.user.id);
-                console.log(req.user);
                 return res.status(200).json({ success: `Logged on as ${user.id}`, user: user, session: req.session, "req.user": req.user })
             });
         })(req, res, next);
     }
 );
+
+router.get("/logout", (req, res) => {
+    req.logOut();
+    console.log("Logout run on the serverside");
+    return res.status(200).json({ success: `Logged out` })
+});
 
 router.get("/test", (req, res) => res.send("hello"))
 
