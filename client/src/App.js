aa031736bdf19330ccc5847add97a5f5c5fa1a72
markgalante <mark.galante.14@gmail.com>
2020-12-05 15:25:08 +0200

    - If logged in - show "logout" button. - If not logged in, show "sign in" button. - Logic added via axios get request to run logout function

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link, Switch, Route, useHistory } from "react-router-dom";
+import { useQuery, useMutation } from "@apollo/client";
+import { CURRENT_USER } from "./graphql/queries";
+import { UPDATE_USER } from "./graphql/mutations";
+import axios from "axios";
 
 //Imports of components 
 import QuizList from "./components/QuizList";
@@ -7,15 +11,45 @@ import CreateQuiz from "./components/CreateQuiz";
 import SignInSignUp from "./components/SignInSignUp";
 
 function App() {
+  const { data, loading, error } = useQuery(CURRENT_USER, { fetchPolicy: "cache-and-network" });
+  const [updateUser] = useMutation(UPDATE_USER);
+  const [isLoggedIn, setUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (data) setUser(data.currentUser);
+    if (error) console.log({ error });
+  }, [data, error]);
+
+  function logOut() {
+    axios.get("/auth/logout", {})
+      .then(res => {
+        console.log("Successful client logout", { res })
+        history.push("/");
+        updateUser({
+          refetchQueries: [{ query: CURRENT_USER }]
+        });
+      })
+      .catch(err => console.log({ err }))
+
+  }
+
   return (
     <div className="App">
       <Router>
         <div>
+          <button onClick={() => console.log({ loading, data, error })}>
+            Test
+          </button>
           <nav>
             <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/create">Create Quiz</Link></li>
-              <li><Link to="/signin">Sign In</Link></li>
+              {
+                !isLoggedIn
+                  ? <li><Link to="/signin">Sign In</Link></li>
+                  : <li><button onClick={logOut}>Sign Out</button></li>
+              }
             </ul>
           </nav>
         </div>
