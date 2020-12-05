6660c3f94f9135373c9af51b6576ee80233ad686
markgalante <mark.galante.14@gmail.com>
2020-12-05 15:22:44 +0200

    Logic to sign in:
    - Use credentials stored in useState hooks.
    - Send credentials via POST to server
    - Run mutation to refetch currentUser query.
    - Return back to "/".

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useHistory } from "react-router-dom"; 
+import { useHistory } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { CURRENT_USER } from "../graphql/queries";
+import { UPDATE_USER } from "../graphql/mutations";
 
 const SignIn = () => {
-    const history = useHistory(); 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const history = useHistory();
+    const [updateUser] = useMutation(UPDATE_USER);
+    const [email, setEmail] = useState("mark@gmail.com");
+    const [password, setPassword] = useState("1234");
     function handleSubmit(e) {
-        e.preventDefault(); 
+        e.preventDefault();
         const userData = {
             email,
             password
         }
         axios
-            .post("/auth/login", userData, {withCredentials: true})
+            .post("/auth/login", userData)
             .then(res => {
+                console.log("res", res.data);
+                updateUser({
+                    refetchQueries: [{ query: CURRENT_USER }]
+                });
                 history.push("/");
-                console.log("RESPONSE", res)
             })
-            .catch(err => console.log("ERROR", err.response))
+            .catch(err => console.log("ERROR WITH LOGIN", err))
     };
+
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <label>Emailaaaa</label>
-                <input type="text" onChange={e => setEmail(e.target.value)} />
+                <label>Email</label>
+                <input type="text" onChange={e => setEmail(e.target.value)} value={email || ""} />
             </div>
             <div>
                 <label>Password</label>
-                <input type="password" onChange={e => setPassword(e.target.value)} />
+                <input type="password" onChange={e => setPassword(e.target.value)} value={password || ""} autoComplete="true" />
             </div>
 
             <button
