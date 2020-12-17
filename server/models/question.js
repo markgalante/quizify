d9f92e4a4473561330a87059bc886db7751e6bf2
markgalante <mark.galante.14@gmail.com>
2020-12-17 21:26:54 +0200

    Question model: refer to creator of question

--- server/models/question.js
+++ server/models/question.js
@@ -1,9 +1,21 @@
-const mongoose = require("mongoose"); 
-const Schema = mongoose.Schema; 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const questionSchema = new Schema({
-    question: String, 
+    question: String,
     quizId: String,
-}); 
+    options: [
+        {
+            option: String,
+            isCorrect: Boolean
+        }
+    ],
+    creator: {
+        id: {
+            type: Schema.Types.ObjectId, 
+            ref: "User"
+        }
+    }
+});
 
 module.exports = mongoose.model("Question", questionSchema); 
