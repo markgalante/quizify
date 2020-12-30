43f0af1aca3d6ad92a046941c147c070e994b2ca
markgalante <mark.galante.14@gmail.com>
2020-12-30 18:56:03 +0200

    Add id prop for questions on quiz query on clientside

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -19,6 +19,7 @@ export const QUIZ = gql`
                 id
             }
             questions{
+                id
                 question
                 options{
                     option
