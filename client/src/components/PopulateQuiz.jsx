85579194b23dada74343aa6e7426f9c18c8a9654
markgalante <mark.galante.14@gmail.com>
2021-01-02 21:03:08 +0200

    Add features to update and delete quiz names

--- client/src/components/PopulateQuiz.jsx
+++ client/src/components/PopulateQuiz.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { useReactiveVar, useMutation } from "@apollo/client";
+import { DELETE_QUIZ } from "../graphql/mutations"
+import { GET_QUIZZES } from "../graphql/queries";
+import { showQuizEdit } from "../cache";
 import Questions from "./Questions";
 import AddQuestion from "./AddQuestion";
+import UpdateQuizName from "./UpdateQuizName";
 
 const PopulateQuiz = ({ quizData }) => {
-    console.log("Passed props to PopulateQuiz", quizData);
+    const [deleteQuiz] = useMutation(DELETE_QUIZ);
+    const quizEdit = useReactiveVar(showQuizEdit);
+    const history = useHistory();
+
+    function deleteNewQuiz() {
+        deleteQuiz({
+            variables: {
+                id: quizData.quiz.id,
+                creator: quizData.quiz.creator.id
+            },
+            refetchQueries: [{ query: GET_QUIZZES }]
+        });
+        history.push("/");
+    }
+
     return (
         <div>
-            <h1>{quizData.quiz.title}</h1>
+            {
+                quizEdit
+                    ? <UpdateQuizName id={quizData.quiz.id} title={quizData.quiz.title} creator={quizData.quiz.creator.id} />
+                    : (<h1>{quizData.quiz.title}
+                        <span onClick={() => showQuizEdit(true)} className="curser-pointer" > &#9998;</span>
+                        <span onClick={deleteNewQuiz} className="curser-pointer" > &#9747;</span>
+                    </h1>)
+            }
+
+
             <div>
                 <AddQuestion
                     id={quizData.quiz.id}
