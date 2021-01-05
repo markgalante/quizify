95c5d6d051945fd4e1eefbdbdc583b5170e8d560
markgalante <mark.galante.14@gmail.com>
2021-01-05 18:23:49 +0200

    Add useEffect to load data from apollo client

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Options from "./Options";
-import { useParams } from "react-router-dom";
 import { showQuestionEdit } from "../cache";
 import { useReactiveVar, useMutation, useQuery } from "@apollo/client";
 import { DELETE_QUESTION } from "../graphql/mutations";
 import { QUIZ, CURRENT_USER } from "../graphql/queries";
 
 import UpdateQuestion from "./UpdateQuestion";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Questions = ({ questions, creator, quizId }) => {
     const [deleteQuestion] = useMutation(DELETE_QUESTION);
     const editQuestion = useReactiveVar(showQuestionEdit);
     const { data: userData } = useQuery(CURRENT_USER);
+    const { data: quizData, loading: quizLoading, error: quizError } = useQuery(QUIZ, { variables: { id: quizId } });
     const [isCreator, setisCreator] = useState(null);
-    const params = useParams();
+    const [quizInfo, setQuestions] = useState(null);
 
     useEffect(() => {
         if (userData.currentUser) {
@@ -23,11 +24,11 @@ const Questions = ({ questions, creator, quizId }) => {
                 setisCreator(false);
             }
         }
-        console.log()
-    }, [userData, creator]);
+        if (quizData) setQuestions(quizData);
+        console.log("useState - questions", { quizInfo });
+    }, [userData, creator, quizInfo, setQuestions, quizData, quizLoading, quizError]);
 
     function handleDeleteQuestion(questionId) {
-        console.log(questionId, userData.currentUser.id)
         deleteQuestion({
             variables: {
                 questionId,
@@ -37,38 +38,41 @@ const Questions = ({ questions, creator, quizId }) => {
         });
     }
     return (
-        <form>
+        <div>
+
             {
-                questions.length ?
-                    questions.map((question, index) => (
-                        <div key={index}>
-                            {
-                                isCreator
-                                    ? (editQuestion
-                                        ? <UpdateQuestion question={question} editQuestion={editQuestion} creator={creator} />
-                                        : (
-                                            <div>
-                                                <h3>
-                                                    {index + 1}. {question.question}
-                                                    <span className="curser-pointer" title="Edit Question" onClick={() => showQuestionEdit(true)}> &#9998;</span>
-                                                    <span className="delete-button" title="Delete Question" onClick={() => handleDeleteQuestion(question.id)}> &#9747;</span>
-                                                </h3>
+                quizLoading
+                    ? <LoadingSpinner />
+                    : !quizInfo
+                        ? <p>Error</p>
+                        : quizInfo.quiz.questions.map((question, index) => (
+                            <div key={index}>
+                                {
+                                    isCreator
+                                        ? (editQuestion
+                                            ? <UpdateQuestion question={question} editQuestion={editQuestion} creator={creator} quizId={quizId} />
+                                            : (
+                                                <div>
+                                                    <h3>
+                                                        {index + 1}. {question.question}
+                                                        <span className="curser-pointer" title="Edit Question" onClick={() => showQuestionEdit(true)}> &#9998;</span>
+                                                        <span className="delete-button" title="Delete Question" onClick={() => handleDeleteQuestion(question.id)}> &#9747;</span>
+                                                    </h3>
 
-                                                <div className="options" onClick={() => {
-                                                    if (isCreator) showQuestionEdit(false)
-                                                }}>
-                                                    <Options options={question.options} creator={creator} questionIndex={index} editQuestion={editQuestion} />
+                                                    <div className="options" onClick={() => {
+                                                        if (isCreator) showQuestionEdit(false)
+                                                    }}>
+                                                        <Options options={question.options} creator={creator} questionIndex={index} editQuestion={editQuestion} />
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        ))
-                                    : <h3>{question.question}</h3>
-                            }
+                                            ))
+                                        : <h3>{question.question}</h3>
+                                }
 
-                        </div>
-                    ))
-                    : <em>No questions asked</em>
+                            </div>
+                        ))
             }
-        </form>
+        </div>
     )
 };
 
