c510879c82afb342bffa16cfc49e1d874a56b90e
markgalante <mark.galante.14@gmail.com>
2021-06-06 20:41:54 +0200

    Clean up: - removed console logs. - removed unnecessary white spaces.

--- client/src/components/QuizList.jsx
+++ client/src/components/QuizList.jsx
@@ -2,15 +2,12 @@ import { useQuery } from "@apollo/client";
 import React, { useEffect, useState } from "react";
 import { SUBMITTED_QUIZZES } from "../graphql/queries";
 import LoadingSpinner from "./LoadingSpinner";
-import SignInSignUp from "./SignInSignUp";
 import Error from "./Error";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+    Link,
+    useHistory
 } from "react-router-dom";
-import SubmittedQuiz from "./SubmittedQuiz";
+import useSearchQuery from "../custom_hooks/useSearchQuery"
 
 const QuizList = () => {
     //Using useQuery above useState only causes useEffect to run once. 
@@ -18,7 +15,6 @@ const QuizList = () => {
     const [quizzes, setQuizzes] = useState(null);
     const [pageLoading, setLoading] = useState(true);
     const [fetchError, setError] = useState(null);
-
 
     useEffect(() => {
         if (!loading) setLoading(false);
@@ -28,28 +24,20 @@ const QuizList = () => {
 
     return (
         <div className="quiz-list">
-            <Router>
-                {pageLoading
-                    ? (<LoadingSpinner />)
-                    : fetchError
-                        ? <Error message={error.message} />
-                        :
-                        quizzes ?
+            {pageLoading
+                ? (<LoadingSpinner />)
+                : fetchError
+                    ? <Error message={error.message} />
+                    :
+                    quizzes ?
+                        <div>
                             <ul>
-                                <Route>
-                                    {quizzes.map(quiz => (
-                                        <li key={quiz.id}><Link to={`/${quiz.id}`}>{quiz.title}</Link></li>
-                                    ))}
-                                </Route>
+                                {quizzes.map(quiz => (
+                                    <li key={quiz.id} className="nav-link"><Link to={`/quiz/${quiz.id}`}>{quiz.title}</Link></li>
+                                ))}
                             </ul>
-                            : null
-                }
-
-                <Switch>
-                    <Route path="/:id"><SubmittedQuiz /></Route>
-                    <Route path="/signinsignup"><SignInSignUp /></Route>
-                </Switch>
-            </Router>
+                        </div>
+                        : null}
         </div>
     )
 };
