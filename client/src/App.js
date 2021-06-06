e1f48b655f5d9a74949be8dd19e0aa1df72cbac8
markgalante <mark.galante.14@gmail.com>
2021-06-06 20:46:20 +0200

    Wholesale changes to app: - Remove console.logs and unused imports/dependents. - Navigate to quiz from home page. - Clean up unneeded white spaces.

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import QuizList from "./components/QuizList";
 import CreateQuiz from "./components/CreateQuiz";
 import SignInSignUp from "./components/SignInSignUp";
 import UserProfile from "./components/UserProfile";
+import SubmittedQuiz from './components/SubmittedQuiz';
 
 function App() {
   const { data, error } = useQuery(CURRENT_USER, { fetchPolicy: "cache-and-network" });
@@ -20,7 +21,6 @@ function App() {
   useEffect(() => {
     if (data) setUser(data.currentUser);
     if (error) console.log({ error });
-    console.log({ isLoggedIn });
   }, [data, error]);
 
   function logOut() {
@@ -40,40 +40,45 @@ function App() {
     <div className="App">
       <Router>
         <nav className="main-nav-bar">
-          {
-            !isLoggedIn
-              ? (
-                <ul className="nav-list">
-                  <li><Link to="/">Home</Link></li>
-                  <li><Link to="/signin">Sign In</Link></li>
-                </ul>
-              )
-              : (<ul className="nav-list">
-                <li><button><Link to="/">Home</Link></button> </li>
-                <li><Link to="/create">Create Quiz</Link></li>
-                <li><Link to={`/${isLoggedIn.id}`} >My Profile</Link></li>
-                <li><button onClick={logOut}>Sign Out</button></li>
-              </ul>
-              )
-          }
+          {!isLoggedIn
+            ? (
+              <ul className="nav-list">
+                <li className="nav-link"><Link to="/signin">Sign In</Link></li>
+              </ul>)
+            : (
+              <ul className="nav-list">
+                <li className="nav-link"><Link to="/">Home</Link> </li>
+                <li className="nav-link"><Link to="/create">Create Quiz</Link></li>
+                <li className="nav-link"><Link to={`/profile/${isLoggedIn.id}`} >My Profile</Link></li>
+                <li className="nav-link" onClick={logOut}>Sign Out</li>
+              </ul>)}
         </nav>
+
         <Switch>
-          <Route exact path="/"><QuizList /></Route>
+          <Route exact path="/">
+            <QuizList />
+          </Route>
           <Route path="/create">
-            {
-              isLoggedIn
-                ? <CreateQuiz />
-                : <Redirect to="/" />
-            }
-          </Route><Route path="/signin"><SignInSignUp /></Route>
-          <Route path="/:profile">
-            {
-              isLoggedIn
-                ? <UserProfile />
-                : <Redirect to="/" />
+            {isLoggedIn
+              ? <CreateQuiz />
+              : <Redirect to="/" />}
+          </Route>
+          <Route path="/signin">
+            {isLoggedIn
+              ? <Redirect to="/" />
+              : <SignInSignUp />}
+          </Route>
+          <Route path="/profile/:profile">
+            {isLoggedIn
+              ? <UserProfile />
+              : <Redirect to="/" />}
+          </Route>
+          <Route path="/quiz/:id" exact={false}>
+            {isLoggedIn
+              ? <SubmittedQuiz />
+              : <Redirect to="/" />
             }
           </Route>
-
         </Switch>
       </Router>
     </div>
