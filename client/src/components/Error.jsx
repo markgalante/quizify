96aaea09bace534f276b5d31d46fa2a7d9de6261
markgalante <mark.galante.14@gmail.com>
2021-06-21 21:02:33 +0200

    Style custom message box

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,11 +1,15 @@
-import React from "react"; 
+import React from "react";
 
-const Error = ({message}) => {
-    return(
-        <div>
-            Error: {message}
+const Error = ({ message }) => {
+    return (
+        <div className="message-box">
+            <div>
+                <h3 className="centre-text">ERROR:</h3>
+                <p className="centre-text">{message}</p>
+            </div>
+
         </div>
-    ); 
-}; 
+    );
+};
 
-export default Error; 
+export default Error;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/text.css";
+import "./styles/messages.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
