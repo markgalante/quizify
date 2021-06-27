ded355bbc6c3bdeae3690ee46b71f92365f98369
markgalante <mark.galante.14@gmail.com>
2021-06-27 17:05:41 +0200

    Question type: - Return if question is from quiz created by user to show correct or incorrect answers.

--- server/graphql/question.js
+++ server/graphql/question.js
@@ -1,6 +1,7 @@
 const graphql = require("graphql");
 
 const Option = require("../models/option");
+const Quiz = require("../models/quiz");
 
 const { GraphQLObjectType, GraphQLList, GraphQLString, GraphQLID, GraphQLBoolean, GraphQLInputObjectType } = graphql;
 
@@ -13,7 +14,18 @@ const QuestionType = types => new GraphQLObjectType({
         options: {
             type: new GraphQLList(types.OptionType),
         },
-    })
+        quizCreator: {
+            type: GraphQLBoolean,
+            resolve: (parent, args, req) => {
+                if (!req.user) return false;
+                return Quiz.findById(parent.quizId)
+                    .then(quiz => {
+                        if (req.user._id.equals(quiz.creatorId)) return true;
+                        else return false;
+                    });
+            }
+        },
+    }),
 });
 
-module.exports = QuestionType; 
+module.exports = QuestionType;
