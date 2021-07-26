ddc5b09e9fc357bf37adc475f139838f4a551a7d
markgalante <mark.galante.14@gmail.com>
2021-07-26 19:51:52 +0200

    Create file for user's unsubmitted quiz

---
+++ client/src/components/MyUnsubmittedQuiz.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { QUIZ_OF_USER, CURRENT_USER } from "../graphql/queries";
+
+//React-Router
+import { useRouteMatch } from "react-router-dom";
+
+import LoadingSpinner from "./LoadingSpinner";
+import Questions from "./unsubmitted-quiz/Questions";
+import QuizTitle from "./unsubmitted-quiz/QuizTitle"
+
+const MyUnsubmittedQuiz = () => {
+    const match = useRouteMatch();
+    const { data: userData } = useQuery(CURRENT_USER);
+    const { data: quizData, loading: quizLoading, error: quizError } = useQuery(QUIZ_OF_USER, { variables: { id: match.params.id } });
+
+    const [questions, setQuestions] = useState(null);
+    const [title, setTitle] = useState(null);
+    const [creator, setCreator] = useState(null);
+    const [quizId, setQuizId] = useState(null);
+
+    useEffect(() => {
+        console.log({ quizData });
+        console.log({ creator })
+        if (quizData) {
+            setQuestions(quizData.quizOfUser.questions);
+            setTitle(quizData.quizOfUser.title);
+            setCreator(quizData.quizOfUser.creator);
+            setQuizId(quizData.quizOfUser.id);
+        };
+    }, [userData, creator, questions, setQuestions, quizData, quizLoading, quizError, title, setTitle, creator, setCreator, quizId, setQuizId]);
+
+    switch (quizLoading) {
+        case true:
+            return <LoadingSpinner />;
+        case false:
+            if (!questions) {
+                return <p>Error</p>
+            } else {
+                return (
+                    <div>
+                        <QuizTitle title={title} creator={creator} />
+                        <Questions questions={questions} creator={creator} quizId={quizId} />
+                    </div>);
+            };
+    };
+};
+
+export default MyUnsubmittedQuiz;
